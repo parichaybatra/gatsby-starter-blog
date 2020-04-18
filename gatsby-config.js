0ef2e00612f217573d8c8c0109c8523720ee71c7
parichaybatra <parichay.batra.dev@gmail.com>
2020-04-19 04:38:22 +0530

    added tracking id for google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-163915687-1",
       },
     },
     `gatsby-plugin-feed`,
